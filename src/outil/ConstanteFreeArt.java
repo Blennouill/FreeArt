f5437e8bb4210027fb954468a4f163fea6e00e6d
Grimbode <kevin.faulhaber@gmail.com>
2015-02-04 23:01:50 +0100

    Update ConstanteFreeArt.java

--- src/outil/ConstanteFreeArt.java
+++ src/outil/ConstanteFreeArt.java
@@ -1,13 +1,13 @@
 package outil;
 
 /**
- * Classe contentant les différents constantes utilisés au sein de l'application.
+ * Classe contentant les diffÃ©rents constantes utilisÃ©s au sein de l'application.
  * @author Kevin Blenner
  *
  */
 public final class ConstanteFreeArt {
 
-	// Nom des différents servlet pour les redirections du servlet principal
+	// Nom des diffÃ©rents servlet pour les redirections du servlet principal
 	public static final String CONSTANTE_NOM_SERVLET_GALLERIE = "/GallerieServlet";
 	public static final String CONSTANTE_NOM_SERVLET_UPLOAD = "/UploadServlet";
 	public static final String CONSTANTE_NOM_SERVLET_UTILISATEUR = "/UtilisateurServlet";
@@ -20,21 +20,22 @@ public final class ConstanteFreeArt {
 	public static final String CONSTANTE_CHEMIN_VUE_CONNEXION = "/connexion.jsp";
 	public static final String CONSTANTE_CHEMIN_VUE_MENU = "/menu.jsp";
 	public static final String CONSTANTE_CHEMIN_VUE_GALLERIE = "/gallerie.jsp";
+	public static final String CONSTANTE_CHEMIN_VUE_PANIER = "/panier.jsp";
 	
-	// Constante liés aux utilisateurs
+	// Constante liÃ©s aux utilisateurs
 	public static final String CONSTANTE_SESSION_UTILISATEUR = "SessionUtilisateur";
 	public static final String CONSTANTE_FORM_CHAMP_NOM = "NomUtilisateur";
 	public static final String CONSTANTE_FORM_CHAMP_MDP = "MotDePasse";
 	
-	// Constante liés aux uploads
+	// Constante liÃ©s aux uploads
 	public static final String CONSTANTE_CHEMIN_UPLOAD = "/images/";
 	public static final int CONSTANTE_TAILLE_TAMPON = 10240; // 10 ko
 	
 	// Constante message
-	public static final String CONSTANTE_SUCCES_CONNEXION = "Vous êtes à présent connectés !";
-	public static final String CONSTANTE_SUCCES_INSCRIPTION = "Votre inscription a bien été prise en compte, veuillez vous connecter...";
-	public static final String CONSTANTE_ERREUR_CONNEXION = "La tentative de connexion a echouée.";
-	public static final String CONSTANTE_ERREUR_INSCRIPTION = "La tentative d'inscription a echouée.";
+	public static final String CONSTANTE_SUCCES_CONNEXION = "Vous Ãªtes Ã  prÃ©sent connectÃ©s !";
+	public static final String CONSTANTE_SUCCES_INSCRIPTION = "Votre inscription a bien Ã©tÃ© prise en compte, veuillez vous connecter...";
+	public static final String CONSTANTE_ERREUR_CONNEXION = "La tentative de connexion a echouÃ©e.";
+	public static final String CONSTANTE_ERREUR_INSCRIPTION = "La tentative d'inscription a echouÃ©e.";
 	
 	// Constante diverse
 	public static final String CONSTANTE_OBJET_MESSAGE= "Message";
