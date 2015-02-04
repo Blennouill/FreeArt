5c5d41794cc213cb7b7154427b49053b4b52d9d3
Grimbode <kevin.faulhaber@gmail.com>
2015-02-04 23:00:03 +0100

    Update PanierServlet.java

--- src/servlets/PanierServlet.java
+++ src/servlets/PanierServlet.java
@@ -27,6 +27,7 @@ public class PanierServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		this.getServletContext().getRequestDispatcher(ConstanteFreeArt.CONSTANTE_CHEMIN_VUE_PANIER).forward( request, response );
 	}
 
 	/**
@@ -34,33 +35,6 @@ public class PanierServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		Cookie[] cookies = request.getCookies();
-		int count = 1;
-		//getCookies returns null if nothing was found
-		if(cookies != null){
-			count += cookies.size();
-		}
-		String image_url = request.getParameter("image_url");
-		//TODO: Check to see if the image_url is a valid url from this server.
-
-		Cookie imageUrlCook = new Cookie("image_url_"+count, image_url);
-		response.addCookie(imageUrlCook);
 	}
 
 }
-
-/*
-VOICI LA FONCTION POUR RECUPERER TOUT LE PANIER DU CLIENT POUR AFFICHER DANS SOIT LA GALLERIE OU/ET LE PANIER
-	Cookie[] cookies = request.getCookies();
-	HashMap<String, Cookie> cookiesMap = new HashMap<String, Cookie>();
-	if(cookies != null){
-		//quelquechose dans le panier
-		for(Cookie c: cookies){
-			cookiesMap.put(c.getName(), c);
-		}
-		//donc voila tout est dans cookiesMap
-	}else{
-		//rien dans le paneir... 
-	}
-	//send cookiesMap a n'importe quel page jsp pour l'affichage
-*/
