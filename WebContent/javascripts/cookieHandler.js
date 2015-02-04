e9ec9086021675181e12ad14b92c1db3a749debc
Grimbode <kevin.faulhaber@gmail.com>
2015-02-04 22:11:45 +0100

    Update cookieHandler.js

--- WebContent/javascripts/cookieHandler.js
+++ WebContent/javascripts/cookieHandler.js
@@ -1,63 +1,103 @@
-//this will only work with jquery plugin for cookie
-//jquery.cookie.js from https://github.com/carhartl/jquery-cookie
+/*
+
+	For this to work correctly the structure of the page has to be of the following:
+	
+	#panier
+		#itemCount
+		#imgPanierContainer
+			#itemCount
+			#panierImgs
+				.img_in_panier
+					<img in small>
+					.deleteImgCookie
+				//repeat
+			#deleteAllImgCookies
+
+	.addPanier
+		<img>
+
+	updateCount(int) //=> 0 sets the count to 0, -1 removes one from count, 1 adds 1 to count
+*/
+
 $(document).ready(function(){
-	//Add image to panier
+	
+	//an image has to be  a child of this container
 	$('.addPanier').on('click', function(){
-		var imgSrc = $(this + '>img').attr('src');
-		var imgName = $(this + '>img').attr('id');
-		//creating the cookie that expires in one day
-		$.cookie(imgName, imgSrc, { expires: 1 });
-		updateItemCount(1);
+		var img = $(this).children("img")
+		var imgSrc = $(img).attr('src');
+		var imgId = $(img).attr('id');
+
+		//adding it as a cookie that expires in one day
+		$.cookie(imgId, imgSrc, { expires: 1 });
+		updateCount();
+		updatePanier(imgId, imgSrc);
 	});
-	//Delete all img cookies
+	//delete all cookies and empty panier
 	$('#deleteAllImgCookies').on('click', function(){
 		var cookies = $.cookie();
-		//TODO: check if this works
-		for(var cookie in cookies) {
-		   var wasDeleted = $.removeCookie(cookie);
-		   console.log('Was deleted: ' + wasDeleted);
+		//looping through all the cookies and deleting them
+		for(var cookie in cookies){
+			var wasDeleted = $.removeCookie(cookie);
+			console.log('Was deleted: ' + wasDeleted);
 		}
-		updateItemCount(-2);
-		//Emptying the panier
-		$('#panier').empty();
+		$('#panierImgs').empty();
+		updateCount(0);
+		showCount();
 	});
-
-	//Delete specific img cookie
 	$('.deleteImgCookie').on('click', function(){
 		var imgId = $(this).attr('id');
 		var wasDeleted = $.removeCookie(imgId);
 		console.log('Was deleted: ' + wasDeleted);
+		if(wasDeleted){
+			updateCount(-1);
+		}else{
+			console.log('An error occured while trying to delete the cookie');
+		}
 		//Removing the img in panier
-		$('#img_in_panier_' + imgId).remove();
-		updateItemCount(-1);
+		$(this).parent().remove();
+		showCount();
 	});
-
-	//Get all images and add them to the panier liste
-
-	if($('#imgPanierContainer').length()){
-		$.each($.cookie(), function(cookie_name, cookie_url){
-			var img = $('<img id="' + cookie_name +'">');
-			img.attr('src', cookie_url);
-			img.appendTo('#imgPanierContainer');
-		});
-	}
-	//===========add item count to element(s)==============
-	//doesn't matter if these elements exist or not
-	var msg = "Vous avez " + getItemCount() + " images dans votre panier";
-	$('#itemCount').text(msg);
-	$('.itemCount').text(msg);
+	//initial load to fill the panier
+	loadPanier();
+	
 });
 
-function updateItemCount(n){
-	var count = $.cooke("itemCount") || 0;
-	if(n == -2){
-		count = 0;
-	}else{
-		count += n;
-	}
-	$.cookie("itemCount", count, { expires: 1 });
+function showCount(){
+	var msg = "Vous avez " + getItemCount() + " images dans votre panier";
+	$('#itemCount').text(msg);	
 }
 function getItemCount(){
 	//returns 0 if it doesn't exist
 	return $.cookie("itemCount") || 0;
+}
+function updateCount(n){
+		//get current count if it exists
+		var count = $.cookie('itemCount') || 0;
+		if(n == 0){
+			//here we remove everything and empty the panier
+			count = 0;
+		}else{
+			count += n;
+		}
+		//create/update the cookie with the new info and set it to expire in one day
+		$.cookie('itemCount', count, { expires: 1 });
+}
+function updatePanier(id, src){
+	var img = $('<img id="panier_' + id + '" class="panierImg">');
+	var div = $('<div class="img_in_panier"></div>');
+	//update img to remove
+	var del = $('<a class="deleteImgCookie"><span>X</span></a>');
+	$(img).attr('src', src);
+	$([$(img), $(del)]).appendTo($(div));
+	$(div).appendTo($('#panierImgs'));
+}
+function loadPanier(){
+	//checking to see if it exists
+	if($('#imgPanierContainer').length){
+		//looping through all the cookies
+		$.each($.cookie(), function(id, src){
+			updatePanier(id, src);
+		});
+		showCount();
+	}
 }
