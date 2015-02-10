e3fdd9397f7477dba95640032d35b657630aef91
Coemgen4 <k.blenner4@gmail.com>
2015-02-10 21:47:29 +0100

    MAJ test

--- src/service/PanierService.java
+++ src/service/PanierService.java
@@ -1,6 +1,13 @@
 package service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 
@@ -16,9 +23,55 @@ public class PanierService {
 	/**
 	 * 
 	 * @param poRequest
-	 * @return
+	 * @param filesURL
 	 */
-	public boolean DownloadImages(HttpServletRequest poRequest){
-		return true;
+	public void DownloadImages(HttpServletResponse poResponse, String cheminDl, ArrayList<String> listeURLImages){
+		//Traitement de la fonction
+		try{
+		    ZipOutputStream zos = new ZipOutputStream(poResponse.getOutputStream()); 
+		    zipDir(cheminDl + "/", zos, listeURLImages); 
+		    zos.close(); 
+		} 
+		catch(Exception e){ 
+		    e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Creation du fichier zip
+	 * @param dir2zip
+	 * @param zos
+	 * @param filesURL
+	 */
+	public ZipOutputStream zipDir(String dir2zip, ZipOutputStream zos, ArrayList<String> listeURLImages){
+		//Déclaration des variables
+		File zipDir = new File(dir2zip); 
+        File f;
+        //Initialisation des variables
+        byte[] readBuffer = new byte[1024]; 
+        int bytesIn = 0;
+        //Traitement de la fonction
+	    try{  
+	        for(int i=0; i<listeURLImages.size(); i++){ 
+	            f = new File(zipDir, listeURLImages.get(i)); 
+	        }
+	        FileInputStream fis = new FileInputStream(zipDir); 
+	        ZipEntry anEntry = new ZipEntry(zipDir.getPath()); 
+	        
+	        //place the zip entry in the ZipOutputStream object 
+	        zos.putNextEntry(anEntry); 
+	        
+	        //now write the content of the file to the ZipOutputStream 
+	        while((bytesIn = fis.read(readBuffer)) != -1){ 
+	            zos.write(readBuffer, 0, bytesIn); 
+	        } 
+	        //close the Stream 
+	        fis.close(); 
+	    }
+	    catch(Exception e) { 
+	        e.printStackTrace(); 
+	    }
+	    //Retour de la fonction
+	    return zos;
 	}
 }
--- src/servlets/PanierServlet.java
+++ src/servlets/PanierServlet.java
@@ -1,6 +1,10 @@
 package servlets;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -41,16 +45,29 @@ public class PanierServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest poRequest, HttpServletResponse poResponse) throws ServletException, IOException {
 		//Declaration des variables
 		PanierService ps = new PanierService();
+		ArrayList<String> listeURLImages = new ArrayList<String>();
+		String cheminTmp = "D:/Privee/Projet/P52/Projet/App/WebContent/tmp/";
 		Message m;
+		String typeFichier;
+		// Lecture du paramètre 'Chemin' passé à la servlet via la déclaration dans le web.xml
+	    String chemin = this.getServletConfig().getInitParameter("Chemin");
 		//Traitement de la fonction
 		if (poRequest.getParameter("action").compareTo(ConstanteFreeArt.CONSTANTE_PANIER_REQUETE_ACTION_TELECHARGER) == 0){
-			if(ps.DownloadImages(poRequest)){
-				m = new Message(ENUMTypeMessage.AfficheSucces.toString(), ConstanteFreeArt.CONSTANTE_SUCCES_TELECHARGEMENT);
-				poRequest.setAttribute(ENUMTypeMessage.AfficheSucces.toString(), true);
-				poRequest.setAttribute(ConstanteFreeArt.CONSTANTE_OBJET_MESSAGE, m);
-				this.getServletContext().getRequestDispatcher(ConstanteFreeArt.CONSTANTE_CHEMIN_VUE_PANIER).forward(poRequest, poResponse);
-			}
+			//Initialisation des arguments
+			//TODO --> Recuperer l'objet JSON et ses URL !
+			//Affectation du type
+			typeFichier = "application/zip";
+			// Initialise la réponse HTTP
+			poResponse.reset();
+			poResponse.setBufferSize(ConstanteFreeArt.CONSTANTE_TAILLE_TAMPON);
+			poResponse.setContentType(typeFichier);
+			poResponse.setHeader( "Content-Disposition", "attachment; filename=\"" + "FreeArtPackImage" + "\"" );
+			ps.DownloadImages(poResponse, cheminTmp, listeURLImages);
+			m = new Message(ENUMTypeMessage.AfficheSucces.toString(), ConstanteFreeArt.CONSTANTE_SUCCES_TELECHARGEMENT);
+			poRequest.setAttribute(ENUMTypeMessage.AfficheSucces.toString(), true);
+			poRequest.setAttribute(ConstanteFreeArt.CONSTANTE_OBJET_MESSAGE, m);
 		}
+		this.getServletContext().getRequestDispatcher(ConstanteFreeArt.CONSTANTE_CHEMIN_VUE_PANIER).forward(poRequest, poResponse);
 	}
 
 }
